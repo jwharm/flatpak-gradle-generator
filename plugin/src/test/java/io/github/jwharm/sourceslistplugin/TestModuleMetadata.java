7725ec3e8216ca9b220e42bb6a5bd8f8f30b5db9
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-09-27 22:51:40 +0200

    Fix testcase of .module handler

--- plugin/src/test/java/io/github/jwharm/sourceslistplugin/TestModuleMetadata.java
+++ plugin/src/test/java/io/github/jwharm/sourceslistplugin/TestModuleMetadata.java
@@ -2,19 +2,117 @@ package io.github.jwharm.sourceslistplugin;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestModuleMetadata {
 
     @Test
-    public void testModuleMetadata() throws IOException {
-        String contents = Files.readString(Path.of("/home/jw/Downloads/kotlin-gradle-plugin-1.8.10.module"));
+    public void testModuleMetadata() {
         var mod = ModuleMetadata.getInstance();
-        var result = mod.process(contents, "gradle76ApiElements");
-        assertEquals("kotlin-gradle-plugin-1.8.10-gradle76.jar", result.destFilename());
+        var result = mod.process(MODULE_FILE, "runtimeElements");
+        assertEquals("cairo-1.16.2.jar", result);
     }
+
+    private static final String MODULE_FILE = """
+            {
+              "formatVersion": "1.1",
+              "component": {
+                "group": "io.github.jwharm.cairobindings",
+                "module": "cairo",
+                "version": "1.16.2",
+                "attributes": {
+                  "org.gradle.status": "release"
+                }
+              },
+              "createdBy": {
+                "gradle": {
+                  "version": "8.3"
+                }
+              },
+              "variants": [
+                {
+                  "name": "apiElements",
+                  "attributes": {
+                    "org.gradle.category": "library",
+                    "org.gradle.dependency.bundling": "external",
+                    "org.gradle.jvm.version": 20,
+                    "org.gradle.libraryelements": "jar",
+                    "org.gradle.usage": "java-api"
+                  },
+                  "files": [
+                    {
+                      "name": "cairo-1.16.2.jar",
+                      "url": "cairo-1.16.2.jar",
+                      "size": 179193,
+                      "sha512": "b01b894f1c218188ab302a2c348164fa886bf702a2ede3c0229997e5e2552f8c6859eb7bdc258a26360b867592740bf2a58c1ff8370727873aefdc8d6030e461",
+                      "sha256": "5f31d80cd97140107849a1217872f1b811a377e3efffccd08704027bd492d3aa",
+                      "sha1": "d20360f7a756ddfc358f54c1a52f3bf94d19d396",
+                      "md5": "cd20934746c086f17ded471e60821708"
+                    }
+                  ]
+                },
+                {
+                  "name": "runtimeElements",
+                  "attributes": {
+                    "org.gradle.category": "library",
+                    "org.gradle.dependency.bundling": "external",
+                    "org.gradle.jvm.version": 20,
+                    "org.gradle.libraryelements": "jar",
+                    "org.gradle.usage": "java-runtime"
+                  },
+                  "files": [
+                    {
+                      "name": "cairo-1.16.2.jar",
+                      "url": "cairo-1.16.2.jar",
+                      "size": 179193,
+                      "sha512": "b01b894f1c218188ab302a2c348164fa886bf702a2ede3c0229997e5e2552f8c6859eb7bdc258a26360b867592740bf2a58c1ff8370727873aefdc8d6030e461",
+                      "sha256": "5f31d80cd97140107849a1217872f1b811a377e3efffccd08704027bd492d3aa",
+                      "sha1": "d20360f7a756ddfc358f54c1a52f3bf94d19d396",
+                      "md5": "cd20934746c086f17ded471e60821708"
+                    }
+                  ]
+                },
+                {
+                  "name": "sourcesElements",
+                  "attributes": {
+                    "org.gradle.category": "documentation",
+                    "org.gradle.dependency.bundling": "external",
+                    "org.gradle.docstype": "sources",
+                    "org.gradle.usage": "java-runtime"
+                  },
+                  "files": [
+                    {
+                      "name": "cairo-1.16.2-sources.jar",
+                      "url": "cairo-1.16.2-sources.jar",
+                      "size": 161855,
+                      "sha512": "91cfd30ad50ae2e71751f2e086460fad5df7d83ab443252bf5b4fb0bccd69acac9688461dc4357df7c2b44efcc5b1417a7a975cab305c93255b7962572533498",
+                      "sha256": "24ec996d113b25c777ef7aa546a4cfbdb05abf32152e2ada91a3a3486d340b01",
+                      "sha1": "4d3c0160736463ec6e255b81f1443cfa70a774dc",
+                      "md5": "b092574c0ee1ece76a511f112d1928dc"
+                    }
+                  ]
+                },
+                {
+                  "name": "javadocElements",
+                  "attributes": {
+                    "org.gradle.category": "documentation",
+                    "org.gradle.dependency.bundling": "external",
+                    "org.gradle.docstype": "javadoc",
+                    "org.gradle.usage": "java-runtime"
+                  },
+                  "files": [
+                    {
+                      "name": "cairo-1.16.2-javadoc.jar",
+                      "url": "cairo-1.16.2-javadoc.jar",
+                      "size": 553344,
+                      "sha512": "d3d48d0c9b5a97ef5eef45fb89d8c2ecf59143eaadaaf3b8f05f597cdf786ed1ceec4e5ea442c172042a20993791d0ec93a0b6a67ebdd617feb9df7531ed4c3f",
+                      "sha256": "e668e20477c39f86ef5c82034644724553a7007c272b7ccf939691d19b81d1ba",
+                      "sha1": "2c0b8392dff3030a84362d42e1e8a6ed46a0fde0",
+                      "md5": "53048ac7abcf4ff9b89fd2dd62a10f57"
+                    }
+                  ]
+                }
+              ]
+            }
+            """;
 }
